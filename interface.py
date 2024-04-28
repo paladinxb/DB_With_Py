f0979f9f99518965b0060d66121695c866c402f2
Илья <96050258+paladinxb@users.noreply.github.com>
2024-04-28 16:45:40 +1000

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,6 +2,22 @@ import tkinter as tk
 import data_insert
 import psycopg2
 import functools
+
+def connect_to_database():
+    try:
+        # Установка соединения с базой данных PostgreSQL
+        conn = psycopg2.connect(
+            dbname="PyPrac",
+            user="postgres",
+            password="1",
+            host="185.166.197.179",
+            port="5432"
+        )
+        return conn
+    except psycopg2.Error as e:
+        print("Ошибка при подключении к базе данных:", e)
+        return None
+
 def open_table_menu():
     table_menu = tk.Toplevel(root)
     table_menu.title("Меню работы с таблицами")
@@ -121,10 +137,137 @@ def drop_column_from_database(conn, table_name, column_name):
 
     finally:
         cur.close()
+conn = psycopg2.connect(dbname="PyPrac", user="postgres", password="1", host="185.166.197.179", port="5432")
+cur = conn.cursor()
 def open_query_menu():
-    pass
-    # Добавить здесь код для открытия меню работы с запросами
+    query_menu = tk.Toplevel(root)
+    query_menu.title("Меню работы с запросами")
+    query_menu.geometry("500x550")
 
+    # Создание метки с заголовком
+    title_label = tk.Label(query_menu, text="Выберите запрос:")
+    title_label.pack(pady=10)
+    query1_button = tk.Button(query_menu, text="1. Получить список всех доступных туров", command=query1)
+    query1_button.pack(pady=5)
+
+    query2_button = tk.Button(query_menu, text="2. Найти все туры в определенное место (например, 'Париж')", command=query2)
+    query2_button.pack(pady=5)
+
+    query3_button = tk.Button(query_menu, text="3. Отобразить туры с определенным типом размещения (например люкс, стандарт, стандарт +)", command=query3)
+    query3_button.pack(pady=5)
+
+    query4_button = tk.Button(query_menu, text="4. Получить список туров с определенным диапазоном цен", command=query4)
+    query4_button.pack(pady=5)
+
+    query5_button = tk.Button(query_menu, text="5. Отобразить туры с датой начала путешествия в определенном месяце", command=query5)
+    query5_button.pack(pady=5)
+
+    query6_button = tk.Button(query_menu, text="6. Найти продажи по фамилии клиента", command=query6)
+    query6_button.pack(pady=5)
+
+    query7_button = tk.Button(query_menu, text="7. Отобразить туры с продолжительностью больше определенного количества дней", command=query7)
+    query7_button.pack(pady=5)
+
+    query8_button = tk.Button(query_menu, text="8. Получить список всех клиентов, купивших тур в определенную страну", command=query8)
+    query8_button.pack(pady=5)
+
+    query9_button = tk.Button(query_menu, text="9. Найти все туры, которые купил определенный клиент", command=query9)
+    query9_button.pack(pady=5)
+
+    query10_button = tk.Button(query_menu, text="10. Найти все туры, которые еще не были проданы", command=query10)
+    query10_button.pack(pady=5)
+
+def print_query_result(rows):
+    if not rows:
+        print("Нет результатов")
+        return
+
+    # Получение максимальной длины каждого столбца
+    max_widths = [max(len(str(value)) for value in row) for row in zip(*rows)]
+
+    # Вывод данных с выравниванием
+    for row in rows:
+        for i, value in enumerate(row):
+            print(str(value).ljust(max_widths[i]), end=" | ")
+        print()
+
+def execute_query(query, params=None):
+    conn = psycopg2.connect(dbname="PyPrac", user="postgres", password="1", host="185.166.197.179", port="5432")
+    cur = conn.cursor()
+    if params:
+        cur.execute(query, params)
+    else:
+        cur.execute(query)
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+# Пример использования
+def query1():
+    query = "SELECT * FROM туры"
+    rows = execute_query(query)
+
+    print_query_result(rows)
+
+def query2():
+    query = "SELECT * FROM Страны"
+    rows = execute_query(query)
+
+    print_query_result(rows)
+
+def query3():
+    accommodation_type = input("Введите тип размещения: ")
+    query = """
+    SELECT * FROM туры
+    JOIN Виды_туров ON туры.id_вида_тура = Виды_туров.Id_вида
+    WHERE Виды_туров.Вид_тура = %s
+    """
+    rows = execute_query(query, (accommodation_type,))
+    print_query_result(rows)
+def query4():
+    min_price = int(input("Введите минимальную цену: "))
+    max_price = int(input("Введите максимальную цену: "))
+    params = (min_price, max_price)
+    query = "SELECT * FROM туры WHERE Цена BETWEEN %s AND %s"
+    rows = execute_query(query, params)
+    print_query_result(rows)
+
+def query5():
+    date = input("Введите дату: ")
+    query = "SELECT * FROM туры WHERE Дата_отправления = %s"
+    params = (date,)
+    rows = execute_query(query, params)
+    print_query_result(rows)
+
+def query6():
+    sec_name = input("Введите фамилию клиента: ")
+    query = "SELECT * FROM Продажи JOIN Клиенты ON Продажи.Id_клиента = Клиенты.Id_клиента WHERE Клиенты.Фамилия = %s"
+    params = (sec_name,)
+    rows = execute_query(query, params)
+    print_query_result(rows)
+
+def query7():
+    num_of_dys = input("Введите количество дней: ")
+    query =  "SELECT * FROM туры WHERE Количество_дней > %s"
+    rows = execute_query(query, (num_of_dys,))
+    print_query_result(rows)
+
+def query8():
+    country = input("Введите количество дней: ")
+    query =  "SELECT DISTINCT Клиенты.* FROM Клиенты JOIN Продажи ON Клиенты.Id_клиента = Продажи.Id_клиента JOIN туры ON Продажи.Id_тура = туры.Id_тура JOIN Страны ON туры.Id_страны = Страны.Id_страны WHERE Страны.Страна = %s "
+    rows = execute_query(query, (country,))
+    print_query_result(rows)
+
+def query9():
+    sec_name = input("Введите фамилию клиента: ")
+    query =  "SELECT * FROM туры JOIN Продажи ON туры.Id_тура = Продажи.Id_тура JOIN Клиенты ON Продажи.Id_клиента = Клиенты.Id_клиента WHERE Клиенты.Фамилия = %s"
+    rows = execute_query(query, (sec_name,))
+    print_query_result(rows)
+
+def query10():
+    query = "SELECT * FROM туры WHERE туры.Id_тура NOT IN (SELECT DISTINCT Id_тура FROM Продажи);"
+    rows = execute_query(query)
+
+    print_query_result(rows)
 # Создание главного окна
 root = tk.Tk()
 root.title("Программа работы с базой данных")
